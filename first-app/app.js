e09d093d755ba9640a996ce7d755a32e5135d85e
Oliver-Gomes <ogomesphoto@gmail.com>
2018-10-11 16:14:15 -0400

    exploring path and os built-in module

--- first-app/app.js
+++ first-app/app.js
@@ -1,1 +1,21 @@
-console.log("First Node ");
+//global objects in Node instead of window as in browser
+// console.log();
+// setTimeout();
+// clearTimeout();
+// setInterval();
+// clearInterval();
+
+// Our create module
+
+// const log = require("./logger.js");
+// log("message");
+
+// Module: Path
+// const path = require("path");
+// var pathObj = path.parse(__filename);
+// console.log(pathObj);
+
+// Module: OS
+const os = require("os");
+var osPlat = os.release();
+console.log(osPlat);
