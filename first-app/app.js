5f5fdf87ad6868fe4f6766b997ee96d854dc82a7
Oliver-Gomes <ogomesphoto@gmail.com>
2018-10-11 16:49:00 -0400

    exploring Events module

--- first-app/app.js
+++ first-app/app.js
@@ -20,11 +20,21 @@
 // console.log(osMethod);
 
 // Module: File System
-const fs = require("fs");
-const files = fs.readdirSync("./");
-console.log(`Result Sync: ${files}`);
+// const fs = require("fs");
+// const files = fs.readdirSync("./");
+// console.log(`Result Sync: ${files}`);
 
-const filesAsyn = fs.readdir("./", function(err, files) {
-  if (err) console.log("Error", err);
-  else console.log("Result Async", files);
+// const filesAsyn = fs.readdir("./", function(err, files) {
+//   if (err) console.log("Error", err);
+//   else console.log("Result Async", files);
+// });
+
+// Module : Events
+const EventEmitter = require("events");
+const emitter = new EventEmitter();
+// Register a listener
+emitter.on("messageLogged", function() {
+  console.log("Listener called");
 });
+// Raise an event
+emitter.emit("messageLogged");
