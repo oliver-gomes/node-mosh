8e4eb9a91cde959d249b0474996137e8c77fbb8b
Oliver-Gomes <ogomesphoto@gmail.com>
2018-10-12 12:41:53 -0400

    express demo get request

---
+++ express-demo/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+
+// methods
+// app.get();
+// app.post();
+// app.put();
+// app.delete();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!!!");
+});
+
+app.get("/api/courses", (req, res) => {
+  res.send([1, 2, 3]);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening to port ${port}....`));
