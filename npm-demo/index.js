9bf6660b7630b951cba50fec2b4db6c3550c3cd7
Oliver-Gomes <ogomesphoto@gmail.com>
2018-10-11 23:12:46 -0400

    new updates

--- npm-demo/index.js
+++ npm-demo/index.js
@@ -1,4 +1,8 @@
-const _ = require("underscore");
+// const _ = require("underscore");
 
-var value = _.contains([1, 2, 3], 4);
-console.log(value);
+// var value = _.contains([1, 2, 3], 4);
+// console.log(value);
+
+const lion = require("lion-lib-403");
+
+console.log(lion.add(4, 3));
